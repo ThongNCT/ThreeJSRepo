f85dc0d69cd5bc77be1cca9f9cc035c93da70274
Brian <briancvn@gmail.com>
2016-10-07 22:20:01 +0700

    HCN

--- ThreeJS/ThreeJS/Scripts/app/app.component.ts
+++ ThreeJS/ThreeJS/Scripts/app/app.component.ts
@@ -1,41 +1,16 @@
-﻿import { Component, ElementRef } from '@angular/core';
-
-declare var $: any;
+﻿import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html'
+  template: `
+    <h1 class="title">Angular Router</h1>
+    <nav>
+      <a routerLink="/crisis-center" routerLinkActive="active">Crisis Center</a>
+      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+      <a routerLink="/login" routerLinkActive="active">Login</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `
 })
-export class AppComponent { 
-  constructor(element: ElementRef) {
-    // $('#carousel').carouFredSel({
-    //   circular: false,
-    //   infinite: false,
-    //   width: '100%',
-    //   height: 405,
-    //   items: 3,
-    //   auto: false,
-    //   prev: '#prev',
-    //   next: '#next',
-    //   scroll: {
-    //     items: 1,
-    //     duration: 1000,
-    //     easing: 'quadratic',
-    //     onBefore: function( data ) {
-    //       data.items.old.find( 'img[src*="-grey"]' ).stop().fadeTo( 500, 1 );
-    //       data.items.old.removeClass( 'selected' );
-    //       data.items.old.stop().animate({
-    //         height: 313
-    //       });
-    //       $('body').css( 'backgroundColor', '#ddd' );
-    //     },
-    //     onAfter: function( data ) {
-    //       openItem( data.items.visible.eq( 1 ) );
-    //     }
-    //   },
-    //   onCreate: function( data ) {
-    //     openItem( data.items.eq( 1 ) );
-    //   }
-    // });
-  }
+export class AppComponent {
 }
