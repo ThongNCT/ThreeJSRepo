201ebd15117978818762fed1ceded7ddf3e04fe6
Brian <briancvn@gmail.com>
2016-10-07 22:46:05 +0700

    HCN

--- ThreeJS/ThreeJS/Scripts/app/app.component.ts
+++ ThreeJS/ThreeJS/Scripts/app/app.component.ts
@@ -2,15 +2,7 @@
 
 @Component({
   selector: 'my-app',
-  template: `
-    <h1 class="title">Angular Router</h1>
-    <nav>
-      <a routerLink="/crisis-center" routerLinkActive="active">Crisis Center</a>
-      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
-      <a routerLink="/login" routerLinkActive="active">Login</a>
-    </nav>
-    <router-outlet></router-outlet>
-  `
+  templateUrl: 'view/app.component.html'
 })
-export class AppComponent {
-}
+
+export class AppComponent {}
